5dc97d46724a06f679370c96885397e575ae042b
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2020-12-16 14:01:48 -0500

    nav bar mobile, responsive to desktop

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./style.css";
+var _ = require("lodash");
 // import Mobile from "./mobile"
 
 export default class Nav extends Component {
@@ -9,23 +10,49 @@ export default class Nav extends Component {
     this.listener = null;
     this.state = {
       status: "top",
-      navToggle: false
+      mobileNavToggle: false
     };
   }
 
   toggleNav = () => {
-   
+   if (this.state.mobileNavToggle) {
+     this.setState({
+         mobileNavToggle: false
+     })
+   } else if (!this.state.mobileNavToggle) {
     this.setState({
-        navToggle: !this.state.navToggle
-    })
+      mobileNavToggle: true
+  })
+   }
   }
 
   componentDidMount() {}
 
+  componentDidUpdate() {
+    window.addEventListener(
+      "resize",
+      _.debounce(() => {
+        if (window.innerWidth > 750) {
+          this.setState({
+            mobileNavToggle: false,
+          });
+          document.getElementById("navvy-bar").className = "header";
+          document.getElementById("menu-toggle").checked = false
+        } 
+        else if (window.innerWidth < 750) {
+          if (this.state.mobileNavToggle) {
+            document.getElementById("menu-toggle").checked = true;
+          }
+        }
+          
+      }, 400)
+    );
+  }
+
   render() {
     return (
       <div className="nav-section">
-        <header class={this.state.navToggle ? 'mobile-header' : 'header'}>
+      <header id="navvy-bar" class={this.state.mobileNavToggle ? 'mobile-header' : 'header'}>
           <nav>
             <ul>
               <li>
